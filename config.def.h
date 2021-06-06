a5c5da7eacbb3fc650be35f8e181c9d147d864b8
Philipp Sauter <sauterp@protonmail.com>
2021-06-06 08:57:19 +0200

    changes for dietrich

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=32" };
+static const char dmenufont[]       = "monospace:size=32";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacrittycmd", NULL };
+static const char *termcmd[]  = { "termcmd", NULL };
 static const char *copyqcmd[]  = { "copyq", "show", NULL };
 static const char *firefoxcmd[]  = { "copyq-to-browser", "firefox", NULL };
 static const char *surfcmd[]  = { "copyq-to-browser", "surf", NULL };
